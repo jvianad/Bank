4d26e8498e651bd5a4684b72c90f4a79a6e0c374
jvianad <jeanvianademares@gmail.com>
2024-06-05 00:32:47 -0500

    Commit 9: se agrega configuracion de swagger para ir documentando la API Rest

--- src/main/java/com/prbank/Bank/controllers/AccountController.java
+++ src/main/java/com/prbank/Bank/controllers/AccountController.java
@@ -2,17 +2,20 @@ package com.prbank.Bank.controllers;
 import com.prbank.Bank.entities.Account;
 import com.prbank.Bank.entities.dto.DepositRequest;
 import com.prbank.Bank.services.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+
 @RestController
 @RequestMapping("api/v1")
 public class AccountController {
     @Autowired
     private AccountService accountService;
     @GetMapping("/accounts")
+    @Operation(summary = "Obtener cuentas", description = "Este endpoint devuelve una lista de las cuentas del banco")
     public ResponseEntity<List<Account>> getAccounts(){
         List<Account> allAccounts = accountService.getAllAccounts();
         if (allAccounts.isEmpty()){
@@ -21,6 +24,7 @@ public class AccountController {
         return ResponseEntity.ok(allAccounts);
     }
     @PostMapping("/user/{idUser}/accounts")
+    @Operation(summary = "Crear cuentas", description = "Este endpoint crea las cuentas del banco")
     public ResponseEntity<Account> createAccount(@PathVariable Long idUser, @RequestBody Account account){
         Account createdAccount = accountService.createAccount(idUser, account);
         if (createdAccount != null){
@@ -30,6 +34,7 @@ public class AccountController {
         }
     }
     @DeleteMapping("/accounts/{idAccount}")
+    @Operation(summary = "Eliminar cuentas por ID", description = "Este endpoint elimina las cuentas del banco por su ID")
     public ResponseEntity<String> deleteAccountById(@PathVariable Long idAccount){
         try{
             accountService.deleteAccount(idAccount);
@@ -39,11 +44,13 @@ public class AccountController {
         }
     }
     @DeleteMapping("/accounts/{idAccount}/pockets/{idPocket}")
+    @Operation(summary = "Eliminar bolsillos asociados ", description = "Este endpoint elimina bolsillos asociados a las cuentas")
     public ResponseEntity<Void> deleteAccountPocket(@PathVariable Long idAccount, @PathVariable Long idPocket){
         accountService.deleteAccountPocket(idAccount, idPocket);
         return ResponseEntity.noContent().build();
     }
     @PostMapping("/accounts/deposit/{accountNumber}")
+    @Operation(summary = "Depositar dinero", description = "Este endpoint deposita dinero en una cuenta")
     public ResponseEntity<String> depositMoney(@PathVariable String accountNumber, @RequestBody DepositRequest account){
         accountService.deposit(accountNumber,account.getAmount());
         return ResponseEntity.ok("Deposito realizado");
